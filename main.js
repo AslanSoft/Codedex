b88b857d6e2ddcbc48439cd1507cef37987cc872
AslanSoft <beggyplayy@gmail.com>
2025-08-21 20:00:44 +0500

    training completed

--- main.js
+++ main.js
@@ -14,9 +14,32 @@
 // arr.unshift("")
 // console.log(arr);
 
+const fruits = [
+  "olma", "banan", "nok", "uzum", "shaftoli", "anor", "apelsin", "mandarin", "kivi", "mango",
+  "limon", "gilos", "olcha", "qulupnay", "malina", "ko'k", "qovun", "tarvuz", "anjir", "xurmo",
+  "banan", "olma", "mango", "nok", "shaftoli", "apelsin", "uzum", "limon", "gilos", "qulupnay",
+  "anor", "kivi", "mandarin", "malina", "ko'k", "anjir", "tarvuz", "qovun", "xurmo", "olcha",
+  "banan", "olma", "mango", "nok", "shaftoli", "apelsin", "uzum", "limon", "gilos", "qulupnay",
+  "anor", "kivi", "mandarin", "malina", "ko'k", "anjir", "tarvuz", "qovun", "xurmo", "olcha",
+  "banan", "olma", "mango", "nok", "shaftoli", "apelsin", "uzum", "limon", "gilos", "qulupnay",
+  "anor", "kivi", "mandarin", "malina", "ko'k", "anjir", "tarvuz", "qovun", "xurmo", "olcha",
+  "banan", "olma", "mango", "nok", "shaftoli", "apelsin", "uzum", "limon", "gilos", "qulupnay"
+];
+
+console.log();
 
 
-for(let i = 0; i < 10; i ++){
-    console.log(i);
+// for(let son = fruits.length; son > 0; son --){
+//     console.log(son, fruits[son]);
+// }
+// for(let item in fruits){
+//     console.log(    fruits[item]);
+    
+// }
+
+for(let item of fruits){
+    if(item == "mandarin"){
+        console.log(item);
+    }
     
 }
